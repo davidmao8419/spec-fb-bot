bfc2f86821418d603f7dd338e122f6d3866ef384
David Mao <davidmao@Davidde-MacBook-Pro.local>
2019-02-10 11:20:03 -0500

    heroku commit

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const
   express = require('express'),
   bodyParser = require('body-parser'),
   app = express().use(bodyParser.json()); // creates express http server
+  request = require('request');
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
@@ -26,6 +27,11 @@ app.post('/webhook', (req, res) => {
         //console.log(webhook_event);
         let sender_psid = webhook_event.sender.id;
         console.log('Sender PSID: ' + sender_psid);
+        if (webhook_event.message) {
+            handleMessage(sender_psid, webhook_event.message);        
+          } else if (webhook_event.postback) {
+            handlePostback(sender_psid, webhook_event.postback);
+          }
       });
   
       // Returns a '200 OK' response to all requests
@@ -36,6 +42,45 @@ app.post('/webhook', (req, res) => {
     }
   
   });
+
+function handleMessage(sender_psid, received_message) {
+
+let response;
+  
+    // Check if the message contains text
+    if (received_message.text) {    
+  
+      // Create the payload for a basic text message
+      response = {
+        "text": `You sent the message: "${received_message.text}". Now send me an image!`
+      }
+    }   
+    // Sends the response message
+    callSendAPI(sender_psid, response);    
+}
+function callSendAPI(sender_psid, response) {
+    // Construct the message body
+    let request_body = {
+      "recipient": {
+        "id": sender_psid
+      },
+      "message": response
+    }
+  
+    // Send the HTTP request to the Messenger Platform
+    request({
+      "uri": "https://graph.facebook.com/v2.6/me/messages",
+      "qs": { "access_token": 'EAAFV6q1mQZCIBAM47amsXEMuXEUPZAwHu33QcYYS1VOqfkWC2AZCKOWtLPnKyqLzfqYdpP1bU9ewMQqmkOpd7LzShufjIwAKhCmOyHIp34zyIhqY3P2THpqd2QtRlOL2ZBK6oANZCoQI9YkwpeJatcZBogGeoSFYRKbL1KfkjJa1chhZCM4Kihz' },
+      "method": "POST",
+      "json": request_body
+    }, (err, res, body) => {
+      if (!err) {
+        console.log('message sent!')
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }); 
+  }
 
   // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
