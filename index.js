563127c1aea5982cdee2af23b568913edefb72a9
David Mao <davidmao@Davidde-MacBook-Pro.local>
2019-02-13 21:52:34 -0500

    heroku commit

--- index.js
+++ index.js
@@ -40,10 +40,9 @@ app.post('/webhook', (req, res) => {
         // Gets the message. entry.messaging is an array, but 
         // will only ever contain one message, so we get index 0
         let webhook_event = entry.messaging[0];
-        //console.log(webhook_event);
+        console.log(webhook_event);
         let sender_psid = webhook_event.sender.id;
-        console.log('Sender PSID: ' + sender_psid);
-        console.log("so weird");
+        //console.log('Sender PSID: ' + sender_psid);
         
         if (webhook_event.message) {
             handleMessage(sender_psid, webhook_event.message);        
