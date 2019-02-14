ea2ac16b98cbc790dfa1cd0eb37d4737730326b1
David Mao <davidmao@Davidde-MacBook-Pro.local>
2019-02-13 22:48:20 -0500

    heroku commit

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ bot.on('message', (payload, chat) => {
 app.post('/webhook', (req, res) => {  
 
     let body = req.body;
-    console.log("testingggggggg");
+    console.log(req);
     // Checks this is an event from a page subscription
     if (body.object === 'page') {
       console.log(body);
