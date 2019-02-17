a5c24c7f4260fbeb2b1887b68e3ff67c7fff442a
David Mao <davidmao@Davidde-MacBook-Pro.local>
2019-02-16 22:39:41 -0500

    heroku commit

--- bot.js
+++ bot.js
@@ -12,9 +12,16 @@ const request = require('request');
 const BootBot = require('bootbot');
 var facebookID;
 var mongoose = require('mongoose');
+var CronJob = require('cron').CronJob;
 var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
 mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 
-console.log("!!!!!!!!!!!!@@@@@@@@@@@@!!!!!!!!!!!!!@@@@@@@@@@@@@");
+console.log('Before job instantiation');
+const job = new CronJob('0 */10 * * * *', function() {
+	const d = new Date();
+	console.log('Every Tenth Minute:', d);
+});
+console.log('After job instantiation');
+job.start();
